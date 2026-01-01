f7dd734ba6cc8da9a8a326d0f8741ec891393842
Archit Tyagi <archittyagi462@gmail.com>
2026-01-01 21:28:27 +0530

    feat: add LeetCode daily challenge folder and template for 2026-01-01

---
+++ contests/leetcode/daily/2026-01-01.cpp
@@ -0,0 +1,21 @@
+// LeetCode Daily Challenge - January 1, 2026
+// Problem: 
+// Link: 
+// Difficulty: 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // Solution function here
+    
+};
+
+int main() {
+    Solution sol;
+    
+    // Test cases
+    
+    return 0;
+}
