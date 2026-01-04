a52501e3dc67af1d160ebfba7007daab63a19797
Archit Tyagi <archittyagi462@gmail.com>
2026-01-04 19:56:43 +0530

    Add LeetCode 1390: Four Divisors solution

---
+++ leetcode_daily/1390.cpp
@@ -0,0 +1,54 @@
+// 1390. Four Divisors
+// Difficulty: Medium
+// Given an integer array nums, return the sum of divisors of the integers 
+// in that array that have exactly four divisors.
+// If there is no such integer in the array, return 0.
+
+// Example 1:
+// Input: nums = [21,4,7]
+// Output: 32
+// Explanation: 
+// 21 has 4 divisors: 1, 3, 7, 21
+// 4 has 3 divisors: 1, 2, 4
+// 7 has 2 divisors: 1, 7
+// The answer is the sum of divisors of 21 only.
+
+// Example 2:
+// Input: nums = [21,21]
+// Output: 64
+
+// Example 3:
+// Input: nums = [1,2,3,4,5]
+// Output: 0
+
+// Constraints:
+// 1 <= nums.length <= 10^4
+// 1 <= nums[i] <= 10^5
+
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int total = 0;
+
+        for (int x : nums) {
+            int cnt = 0;
+            int sum = 0;
+
+            for (int i = 1; i * i <= x; i++) {
+                if (x % i == 0) {
+                    int j = x / i;
+                    if (i == j) {
+                        cnt++;
+                        sum += i;
+                    } else {
+                        cnt += 2;
+                        sum += i + j;
+                    }
+                    if (cnt > 4) break;
+                }
+            }
+            if (cnt == 4) total += sum;
+        }
+        return total;
+    }
+};
