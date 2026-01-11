46116656254fdf6621ad2b63bfb57eea5d3d396d
Archit Tyagi <archittyagi462@gmail.com>
2026-01-11 08:44:49 +0530

    Add LeetCode 85: Maximal Rectangle

---
+++ leetcode_daily/85.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int n = heights.size();
+        int maxArea = 0;
+
+        for (int i = 0; i <= n; i++) {
+            int h = (i == n ? 0 : heights[i]);
+
+            while (!st.empty() && h < heights[st.top()]) {
+                int height = heights[st.top()];
+                st.pop();
+
+                int width;
+                if (st.empty())
+                    width = i;
+                else
+                    width = i - st.top() - 1;
+
+                maxArea = max(maxArea, height * width);
+            }
+            st.push(i);
+        }
+        return maxArea;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if (matrix.empty()) return 0;
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        vector<int> height(cols, 0);
+        int ans = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1')
+                    height[j]++;
+                else
+                    height[j] = 0;
+            }
+            ans = max(ans, largestRectangleArea(height));
+        }
+        return ans;
+    }
+};
